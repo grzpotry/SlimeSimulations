c4b3fcf086533565a8941c3d5c4e7a8dfc7cac2c
grzpotry <grzpotry@gmail.com>
2022-04-10 18:21:03 +0200

    wip

--- Assets/Simulation.cs
+++ Assets/Simulation.cs
@@ -6,39 +6,44 @@ public class Simulation : MonoBehaviour
     public readonly struct Agent
     {
         public Vector2 Position { get; }
-        public float Angle { get; }
+        public float AngleRad { get; }
 
         public static int SizeOf => System.Runtime.InteropServices.Marshal.SizeOf(typeof(Agent));
 
-        public Agent(Vector2 position, float angle)
+        public Agent(Vector2 position, float angleRad)
         {
             Position = position;
-            Angle = angle;
+            AngleRad = angleRad;
         }
     }
 
     protected void Awake()
     {
-        _renderTexture = new RenderTexture(_width, _height, 24);
-        _renderTexture.enableRandomWrite = true;
-        _renderTexture.Create();
+        _rawTexture = new RenderTexture(_width, _height, 24);
+        _rawTexture.enableRandomWrite = true;
+        _rawTexture.Create();
+
+        _diffusedTexture = new RenderTexture(_width, _height, 24);
+        _diffusedTexture.enableRandomWrite = true;
+        _diffusedTexture.Create();
 
         _mainKernelIndex = _computeShader.FindKernel("UpdateMovement");
-        _computeShader.SetTexture(_mainKernelIndex, "Result", _renderTexture);
+        _computeShader.SetTexture(_mainKernelIndex, "Result", _rawTexture);
 
         _senseKernelIndex = _computeShader.FindKernel("UpdateSensors");
-        _computeShader.SetTexture(_senseKernelIndex, "Result", _renderTexture);
-
-        _evaporateKernelIndex = _computeShader.FindKernel("EvaporateTrail");
-        _computeShader.SetTexture(_evaporateKernelIndex, "Result", _renderTexture);
+        _computeShader.SetTexture(_senseKernelIndex, "Result", _rawTexture);
 
         _clearKernelIndex = _computeShader.FindKernel("Clear");
-        _computeShader.SetTexture(_clearKernelIndex, "Result", _renderTexture);
+        _computeShader.SetTexture(_clearKernelIndex, "Result", _rawTexture);
 
         _blurKernelIndex = _computeShader.FindKernel("Blur");
-        _computeShader.SetTexture(_blurKernelIndex, "Result", _renderTexture);
+        _computeShader.SetTexture(_blurKernelIndex, "DiffusedTex", _diffusedTexture);
+        _computeShader.SetTexture(_blurKernelIndex, "Result", _rawTexture);
 
-        _outputRenderer.material.mainTexture = _renderTexture;
+        _evaporationKernelIndex = _computeShader.FindKernel("Evaporation");
+        _computeShader.SetTexture(_evaporationKernelIndex, "Result", _rawTexture);
+
+        _outputRenderer.material.mainTexture = _diffusedTexture;
 
         if (_agents == null || _agents.Length != _agentsCount)
         {
@@ -47,16 +52,33 @@ public class Simulation : MonoBehaviour
 
         _computeShader.SetFloat("width", _width);
         _computeShader.SetFloat("height", _height);
+
+        RunSimulation();
+    }
+
+    protected void OnGUI()
+    {
     }
 
     protected void Update() //todo: Fixed
+    {
+        RunSimulation();
+        foreach (var agent in _agents)
+        {
+            var heading = new Vector2(Mathf.Cos(agent.AngleRad), Mathf.Sin(agent.AngleRad));
+            var end = agent.Position + heading * +_debugHeadingRayLength;
+            Debug.DrawLine(agent.Position, end, Color.red);
+        }
+    }
+
+    private void RunSimulation()
     {
         _computeShader.SetFloat("deltaTime", Time.fixedDeltaTime);
         _computeShader.SetFloat("time", Time.time);
         _computeShader.SetFloat("speed", _agentsSpeed);
         _computeShader.SetFloat("evaporationSpeed", _evaporationSpeed);
         _computeShader.SetInt("sensorWidth", _sensorWidth);
-        _computeShader.SetFloat("sensorAngle", _sensorAngle);
+        _computeShader.SetFloat("sensorAngle", _sensorAngleDeg * Mathf.Deg2Rad);
         _computeShader.SetFloat("sensorOffsetDistance", _sensorOffsetDistance);
         _computeShader.SetFloat("diffuseSpeed", _diffuseSpeed);
 
@@ -72,6 +94,7 @@ public class Simulation : MonoBehaviour
             }
         }
 
+        _computeShader.Dispatch(_evaporationKernelIndex, _width, _height, 1);
         _computeShader.Dispatch(_blurKernelIndex, _width, _height, 1);
     }
 
@@ -81,7 +104,11 @@ public class Simulation : MonoBehaviour
 
         for (int i = 0; i < _agentsCount; i++)
         {
-            _agents[i] = new Agent(GetRandomPosition(), (float)_random.NextDouble() * 2 * Mathf.PI);
+            var randomPosition = GetRandomPosition();
+            var center = new Vector2((_width / 2.0f), (_height / 2.0f));
+            var heading = (center - randomPosition).normalized;
+            var angleRad = Mathf.Atan2(heading.y, heading.x);
+            _agents[i] = new Agent(randomPosition, angleRad);
         }
 
         _buffer?.Dispose();
@@ -94,6 +121,9 @@ public class Simulation : MonoBehaviour
 
     [SerializeField]
     private int _width = 256;
+
+    [SerializeField]
+    private int _debugHeadingRayLength = 2;
 
     [SerializeField]
     private int _stepsPerFrame = 1;
@@ -114,10 +144,12 @@ public class Simulation : MonoBehaviour
     private MeshRenderer _outputRenderer;
 
     [SerializeField]
-    private RenderTexture _renderTexture;
+    private RenderTexture _rawTexture;
+
+    private RenderTexture _diffusedTexture;
 
     [SerializeField]
-    private float _sensorAngle;
+    private float _sensorAngleDeg;
 
     [SerializeField]
     private int _sensorWidth;
@@ -134,11 +166,11 @@ public class Simulation : MonoBehaviour
     [SerializeField]
     private bool _sensoryStage = true;
 
+    private int _evaporationKernelIndex;
     private int _blurKernelIndex;
     private int _senseKernelIndex;
     private int _mainKernelIndex;
     private int _clearKernelIndex;
-    private int _evaporateKernelIndex;
     private readonly Random _random = new Random();
 
     private Agent[] _agents;
@@ -149,10 +181,10 @@ public class Simulation : MonoBehaviour
 
     float RandomSign() => (float)_random.NextDouble() > 0.5f ? 1 : -1;
 
-    private Vector2Int GetRandomPosition()
+    private Vector2 GetRandomPosition()
     {
         int x = _random.Next(0, _width);
         int y = _random.Next(0, _height);
-        return new Vector2Int(x, y);
+        return new Vector2(x, y);
     }
 }
